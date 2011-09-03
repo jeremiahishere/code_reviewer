8123aebf8c8eb6e3c68e89bef56f2e14e489bb51
jeremiahishere <jeremiah@cloudspace.com>
2011-09-03 08:23:49 -0400

    Setup devisable user and started working on application layout.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,5 +22,8 @@ CodeReviewer::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+
+  # devise configuration
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
 
