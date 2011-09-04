3e8ccbeb9edd5ff436e466179d09fb0aba079992
jeremiahishere <jeremiah@cloudspace.com>
2011-09-04 19:13:00 -0400

    Setup the diff formatting.  Still missing line numbers but this is a step in the right direction.

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,27 @@
 module ReviewsHelper
+  def display_diff(diff_text)
+    disp_text = ""
+    started = false
+    diff_text.each_line  do |line|
+      if line.match("---")
+        if started
+          disp_text += "</pre></div>"
+        else
+          started = true
+        end
+        disp_text += "<div class='codeblock'><pre>"
+      end
+      # if the line starts with whitespace
+      line.gsub!(/\n/, "")
+      if line.match(/^\s*-/) && !line.match(/---/)
+        disp_text += "<div class='removed_code'>" + html_escape(line) + "</div>"
+      elsif line.match(/^\s*\+/) && !line.match(/\+\+\+/)
+        disp_text += "<div class='added_code'>" + html_escape(line) + "</div>"
+      else
+        disp_text += "<div class='unchanged_code'>" + html_escape(line) + "</div>"
+      end
+    end
+    disp_text += "</pre></div>"
+    return disp_text.html_safe
+  end
 end
