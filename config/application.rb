0d17cdb80f23ea92f26dc2469d7188ae43fc615e
jeremiahishere <jeremiah@cloudspace.com>
2011-09-15 18:30:20 -0400

    Possible fix for the time zone issue.  Not sure if this will fix the problem on the production server.

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,9 @@ module CodeReviewer
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
+    # Fix a fun time zone issue, don't know if this is the correct fix
+    config.active_record.default_timezone = :local
+
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
