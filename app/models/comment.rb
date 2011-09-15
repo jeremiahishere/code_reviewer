bc2ccb130ed64b206e314ecdc189d8f80cfefea8
jeremiahishere <jeremiah@cloudspace.com>
2011-09-15 18:21:08 -0400

    Added a default ordering for commments by post date instead of using the ids.  In some cases, the post date order does not match the id order.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,6 +7,8 @@ class Comment < ActiveRecord::Base
 
   validates_presence_of :review_submission_id, :user_id, :post_date, :comment
 
+  default_scope :order => :post_date
+
   def review
     self.review_submission.review
   end
